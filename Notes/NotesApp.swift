b4bc7fd7610e800301ff649ae54a5b0db65867f4
YASHAVIKA SINGH <YASHAVIKA.SINGH@GMAIL.COM>
2024-05-28 17:38:36 +0000

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Yashavika Singh on 28.05.24.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
